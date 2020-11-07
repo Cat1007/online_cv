d42f6371d25ae81184384ec1c8df5deaad47b91a
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2020-11-07 14:41:17 +0800

    增加点击按钮复制到剪贴板功能；增加PC端动画效果
    复制成功的消息提示尚未完成

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,23 +1,35 @@
 <template>
   <v-container fluid class="fill-height">
+    <v-alert dense type="info" class="alert">已复制到剪贴板</v-alert>
     <v-row justify="center" align="center">
       <v-col cols="12" sm="6" lg="3">
         <v-container class="primary--text">
           <v-row>
             <v-col align="center">
-              <v-avatar class="elevation-2" size="80px" color="white">
-                <v-icon color="green" size="60px">mdi-phone</v-icon>
-              </v-avatar>
+              <v-hover>
+                <template v-slot="{ hover }">
+                  <v-avatar
+                    id="phone"
+                    class="transition-swing"
+                    size="80px"
+                    color="white"
+                    v-on:click="copyToClipboard($event)"
+                    :class="`elevation-${hover ? 8 : 2}`"
+                  >
+                    <v-icon color="green" size="60px">mdi-phone</v-icon>
+                  </v-avatar>
+                </template>
+              </v-hover>
             </v-col>
           </v-row>
           <v-row>
             <v-col align="center">
               <v-row justify="center" no-gutters>
-                <v-col cols="12" class="title font-weight-medium">
-                  电话
-                </v-col>
-                <v-col cols="12" >
-                  <span class="title font-weight-medium">15700000000</span>
+                <v-col cols="12" class="title font-weight-medium"> 电话 </v-col>
+                <v-col cols="12">
+                  <span id="phoneText" class="title font-weight-medium"
+                    >15700000000</span
+                  >
                 </v-col>
               </v-row>
             </v-col>
@@ -29,19 +41,30 @@
         <v-container class="primary--text">
           <v-row>
             <v-col align="center">
-              <v-avatar class="elevation-2" size="80px" color="white">
-                <v-icon color="orange" size="60px">mdi-email</v-icon>
-              </v-avatar>
+              <v-hover>
+                <template v-slot="{ hover }">
+                  <v-avatar
+                    id="email"
+                    class="transition-swing"
+                    size="80px"
+                    color="white"
+                    v-on:click="copyToClipboard($event)"
+                    :class="`elevation-${hover ? 8 : 2}`"
+                  >
+                    <v-icon color="orange" size="60px">mdi-email</v-icon>
+                  </v-avatar>
+                </template>
+              </v-hover>
             </v-col>
           </v-row>
           <v-row justify="center">
             <v-col align="center">
               <v-row justify="center" no-gutters>
-                <v-col cols="12"  class="title font-weight-medium">
-                  邮箱
-                </v-col>
-                <v-col cols="12" >
-                  <span class="title font-weight-medium">949494945@qq.com</span>
+                <v-col cols="12" class="title font-weight-medium"> 邮箱 </v-col>
+                <v-col cols="12">
+                  <span id="emailText" class="title font-weight-medium"
+                    >949494945@qq.com</span
+                  >
                 </v-col>
               </v-row>
             </v-col>
@@ -53,9 +76,20 @@
         <v-container class="primary--text">
           <v-row>
             <v-col align="center">
-              <v-avatar class="elevation-2" size="80px" color="white">
-                <v-icon color="black" size="60px">mdi-github</v-icon>
-              </v-avatar>
+              <v-hover>
+                <template v-slot="{ hover }">
+                  <v-avatar
+                    id="github"
+                    class="transition-swing"
+                    size="80px"
+                    color="white"
+                    v-on:click="copyToClipboard($event)"
+                    :class="`elevation-${hover ? 8 : 2}`"
+                  >
+                    <v-icon color="black" size="60px">mdi-github</v-icon>
+                  </v-avatar>
+                </template>
+              </v-hover>
             </v-col>
           </v-row>
           <v-row justify="center">
@@ -65,7 +99,9 @@
                   GitHub
                 </v-col>
                 <v-col cols="12">
-                  <span class="title font-weight-medium">XXXXXX</span>
+                  <span id="githubText" class="title font-weight-medium"
+                    >XXXXXX</span
+                  >
                 </v-col>
               </v-row>
             </v-col>
@@ -77,19 +113,32 @@
         <v-container class="primary--text">
           <v-row>
             <v-col align="center">
-              <v-avatar class="elevation-2" size="80px" color="white">
-                <v-icon color="red" size="60px">mdi-sina-weibo</v-icon>
-              </v-avatar>
+              <v-hover>
+                <template v-slot="{ hover }">
+                  <v-avatar
+                    id="weibo"
+                    class="transition-swing"
+                    size="80px"
+                    color="white"
+                    v-on:click="copyToClipboard($event)"
+                    :class="`elevation-${hover ? 8 : 2}`"
+                  >
+                    <v-icon color="red" size="60px">mdi-sina-weibo</v-icon>
+                  </v-avatar>
+                </template>
+              </v-hover>
             </v-col>
           </v-row>
           <v-row>
             <v-col align="center">
               <v-row justify="center" no-gutters>
-                <v-col cols="12"  class="title font-weight-medium">
+                <v-col cols="12" class="title font-weight-medium">
                   新浪微博
                 </v-col>
-                <v-col cols="12" >
-                  <span class="title font-weight-medium">XXXXXX</span>
+                <v-col cols="12">
+                  <span id="weiboText" class="title font-weight-medium"
+                    >XXXXXX</span
+                  >
                 </v-col>
               </v-row>
             </v-col>
@@ -103,8 +152,26 @@
 <script>
 export default {
   name: "Contact",
+  methods: {
+    copyToClipboard: function (event) {
+      var toCopy = document.getElementById(event.currentTarget.id + "Text")
+        .innerText;
+      console.log(event.currentTarget.id + ":" + toCopy);
+      //execCommand只能复制类似于input，textarea等的内容，创建一个临时标签
+      var tempTextarea = document.createElement("textarea");
+      document.body.appendChild(tempTextarea);
+      tempTextarea.value = toCopy;
+      tempTextarea.select();
+      document.execCommand("Copy");
+      document.body.removeChild(tempTextarea);
+      alert("已复制到剪贴板");
+    },
+  },
 };
 </script>
 
 <style scoped>
+.alert{
+  position: absolute;
+}
 </style>
